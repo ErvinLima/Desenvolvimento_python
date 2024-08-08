635ecb228fa5edfb32362c4a8ef24b0b4b059d3f
Ervinlima <ervinlima41@gmail.com>
2024-08-08 10:58:45 -0300

    entrando em listas

---
+++ Exemplos e Explicação/Listas.py
@@ -0,0 +1,10 @@
+# Colchetes [] indicam uma lista, seus elementos sao separados por virgula.
+
+bicycles = ['trek', 'cannodale', 'redline', 'specialized']
+print(bicycles)
+
+# Listas são ordenaadas, portanto você consegue acessar qualquer elemento informando a posição que ele se encontra. 
+# OBS: os elementos nunca começam na posição 1, sempre no 0.
+
+print(bicycles[2])
+# Aqui solicitei o item 2 da lista e ele retorna apenas o elemento presente naquela posicao 
---
+++ Exercícios/Número favorito.py
@@ -0,0 +1,3 @@
+favorite_number = 21
+
+print("O numero favorito do ervin eh: " + str(favorite_number))
