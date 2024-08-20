389301a1db4c7670befd543593a8b39c2c35972d
Ervinlima <ervinlima41@gmail.com>
2024-08-20 15:37:39 -0300

    aplicando o conhecimento

---
+++ Exercícios/Pizzas.py
@@ -0,0 +1,6 @@
+sabores = ['peito de peru', 'quatro queijos', 'carne seca']
+
+for sabor in sabores:
+    print("Esse eh um dos meus sabores de pizza preferido: " + sabor.title() + '\n')
+
+print("Esses sabores sao os meus favoritos, porem quando estou no rodizio, muitos sabores me apetecem.")
