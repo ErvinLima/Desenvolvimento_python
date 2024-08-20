4c11703393a17dc5e8ff8c5042e816f4e53c8f1a
Ervinlima <ervinlima41@gmail.com>
2024-08-20 15:47:18 -0300

    aplicando o conhecimento

---
+++ Exercícios/Animais.py
@@ -0,0 +1,7 @@
+animais = ['vaca', 'cavalo', 'camelo', 'bode']
+
+for animal in animais:
+    print(animal.upper())
+    print('o ' + animal.title() + ' seria um otimo animal para producao\n')
+
+print('Qualquer um desses animais seria otimo para producao, todos eles tem sua peculiaridade para o desenvolvimento agricula')
